e7d40a56105511436b7a95afb9724a5bd6d8b3bc
Byron Petselis <byron.petselis@student.unsw.edu.au>
2023-12-02 01:38:46 +1100

    Changes to Pipeline

--- Running_Pipeline/SVM_Train.py
+++ Running_Pipeline/SVM_Train.py
@@ -17,24 +17,28 @@ def svm_train(train_data, test_data, train_target, test_target, gamma_value):
 
 #     for gamma_value in testing_range:
 #     cls = svm.SVC(kernel='rbf', gamma=0.3, C=1, verbose=1)
+  
     cls = svm.SVC(kernel='rbf', gamma=gamma_value, C=1, verbose=1)
     cls.fit(train_data, train_target)
     pred = cls.predict(test_data)
-    accuracy.append(metrics.accuracy_score(test_target,pred))
-    # Accuracy: How many were right?
-    precision.append(metrics.precision_score(test_target,pred))
-    # Precision: Out of all '1' predictions, how many were right?
-    recall.append(metrics.recall_score(test_target,pred))
-    # Recall: Out of all '1' truths, how many were right?
+    # accuracy.append(metrics.accuracy_score(test_target,pred))
+    # precision.append(metrics.precision_score(test_target,pred))
+    # recall.append(metrics.recall_score(test_target,pred))
 
-#     plt.figure(figsize=(12, 8))
+    print("accuracy:", metrics.accuracy_score(test_target,pred))
+    print()
+    print("precision:", metrics.precision_score(test_target,pred))
+    print()
+    print("recall:", metrics.recall_score(test_target,pred))
+    print()
+    print(metrics.classification_report(test_target,pred))
 
-#     plt.plot(testing_range, accuracy, color='r', label='accuracy')
-#     plt.plot(testing_range, precision, color='g', label='precision')
-#     plt.plot(testing_range, recall, color='b', label='recall')
-
-#     plt.xlabel("RBF Kernel Value")
-#     plt.ylabel("Metrics Report")
-#     plt.title("Testing Various RBF Values")
-
-#     plt.legend()
+  
+    # plt.figure(figsize=(12, 8))
+    # plt.plot(accuracy, color='r', label='accuracy')
+    # plt.plot(precision, color='g', label='precision')
+    # plt.plot(recall, color='b', label='recall')
+    # plt.xlabel("RBF Kernel Value")
+    # plt.ylabel("Metrics Report")
+    # plt.title("Testing Various RBF Values")
+    # plt.legend()
